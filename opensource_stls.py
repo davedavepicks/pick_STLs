8db6ffe89c274c796a76ec55653d596741ca5914
davedavemckay <d.mckay@epcc.ed.ac.uk>
2025-10-28 12:33:32 +0000

    Add script to download and publish (opensource) STLs from Google Drive.

--- opensource_stls.py
+++ opensource_stls.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
 import os
 import io
 import requests
@@ -5,8 +9,8 @@ import pandas as pd
 import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
-from google.auth.credentials import Credentials as GoogleCredentials
 import json
+from tqdm import tqdm
 
 API_BASE = "https://api.github.com"
 API_HEADERS = {
@@ -53,21 +57,24 @@ def fetch_private_github_file(
 def load_csv_from_bytes(content: bytes) -> pd.DataFrame:
     return pd.read_csv(io.BytesIO(content))
 
-def ls_drive_folder(auth_json_dict: dict, folder_id: str, recursive: bool = False, depth: str|int = 'full', level: int = 0) -> tuple[list[dict],list[dict]]:  #,list[dict]]:
+def gdrive_stls(auth_json_dict: dict, folder_id: str, recursive: bool = False, depth: str|int = 'full', _level: int = 0) -> dict:
     """
-    Lists files in a Google Drive folder using the Drive API.
+    Finds STL files in a Google Drive folder using the Drive API.
+    Optionally recursive down to a given numerical depth or 'full' depth.
 
     Args:
         auth_json_dict: A dictionary containing the JSON-formatted Google Drive
                         service account or OAuth 2.0 client credentials.
         folder_id: The ID of the Google Drive folder to list files from.
-        recursive: If True, lists files in subfolders recursively (not implemented yet).
+        recursive: If True, lists files in subfolders recursively.
+        depth: Numerical depth or 'full' (default = 'full').
+        _level: used internally during recursion.
     Returns:
-        A list of dictionaries, each representing a file in the folder.
+        A dictionary of STL files in the folder.
     """
-    # items = []
-    stls = []
-    folders = []
+
+    stls = {}
+    folders = {}
 
     try:
         # Authenticate using the provided JSON dictionary
@@ -95,51 +102,47 @@ def ls_drive_folder(auth_json_dict: dict, folder_id: str, recursive: bool = Fals
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
+
         # recursion through subfolders
         for file in results.get('files', [{}]):
             if file['mimeType'] == 'application/vnd.google-apps.folder':
-                folders.append(file)
+                folders[file['id']] = file
             elif file['mimeType'] == 'application/vnd.ms-pki.stl':
-                stls.append(file)
-            # else:
-            #     items.append(file)
+                stls[file['id']] = file
         
         if recursive:
             if isinstance(depth, int):
-                if level == depth:
+                if _level == depth:
                     if len(folders) > 0:
                         print(f'Warning: stopping recursion at folder {folders[-1]['name']} due to depth of {depth} despite {len(folders)} folders remaining.', end='\r')
-                    return folders, stls  #, items
+                    return stls
             if len(folders) > 0:
                 for f in folders:
-                    subfolders, sub_stls = ls_drive_folder(auth_json_dict, f['id'], recursive, depth, level=level+1)  # , sub_items
+                    subfolders, sub_stls = gdrive_stls(auth_json_dict, f['id'], recursive, depth, _level=_level+1)  # , sub_items
                     if len(subfolders) == 0:
                         pass
-                        # print(f'No subfolders remaining in folder {f['name']} at level {level+1}.                  ', end='\r')
-                    folders.extend(subfolders)
-                    stls.extend(sub_stls)
-                    # items.extend(sub_items)
-        # print('\nDone.')
-        
-        for item in stls+folders:
-            if 'parent_folder' not in item.keys():
-                pid = item['parents'][0]
-                print(pid)
+                        print(f'No subfolders remaining in folder {f['name']} at level {_level+1}.                  ', end='\r')
+
+                    for sf in subfolders:
+                        folders[sf['id']] = sf
+                    for sstl in sub_stls:
+                        stls[sstl['id']] = sstl
+
+        for gd_item in {**stls, **folders}.values():
+            if 'parent_folder' not in gd_item.keys():
+                pid = gd_item['parents'][0]
                 for folder in folders:
                     parent_folder = ''
                     if folder['id'] == pid:
-                        print(folder['id'])
-                        print(folder['name'])
                         parent_folder = folder['name']
-                        item['parent_folder'] = parent_folder
+                        gd_item['parent_folder'] = parent_folder
                         break
                 continue
 
-        return folders, stls  #, items
+        return stls  #, items
 
     except Exception as e:
-        print(f"An error occurred while listing files in the folder: {e}")
-        return [{}], [{}]  #, [{}]
+        raise ValueError(f'Folder ID {folder_id} not found on Google Drive. Does not exist, or permissions do not allow read.')
 
 
 def download_drive_file(auth_json_dict: dict, drive_file_url: str, output_filepath: str):
@@ -150,7 +153,7 @@ def download_drive_file(auth_json_dict: dict, drive_file_url: str, output_filepa
         auth_json_dict: A dictionary containing the JSON-formatted Google Drive
                         service account or OAuth 2.0 client credentials.
         drive_file_url: The URL of the Google Drive file to download,
-                        e.g., 'https://drive.google.com/file/d/1zcGAGkwa_QnVY1_tqsgfQbfEE-cKWLS3'.
+                        e.g., 'https://drive.google.com/file/d/XXXXXXXXX_XXXXXX_XXXXXXXXX-XXXXXX'.
         output_filepath: The local path where the downloaded file will be saved.
     """
     try:
@@ -176,22 +179,25 @@ def download_drive_file(auth_json_dict: dict, drive_file_url: str, output_filepa
             auth_json_dict,
         )[0]
         
-        # Build the Drive API service
         drive_service = build('drive', 'v3', credentials=credentials)
 
         # Request file metadata to get the file name and type (optional, but good practice)
-        file_metadata = drive_service.files().get(fileId=file_id, fields='name').execute()
+        file_metadata = drive_service.files().get(fileId=file_id, fields='name, size').execute()
         file_name = file_metadata.get('name')
+        file_size = int(file_metadata.get('size', 0))
         print(f"Downloading file: '{file_name}'")
 
         # Download the file
         request = drive_service.files().get_media(fileId=file_id)
         fh = io.FileIO(output_filepath, 'wb')
         downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print(f"Download {int(status.progress() * 100)}%.", end="\r")
+        
+        with tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name, ascii=True) as pbar:
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                if status:
+                    pbar.update(status.resumable_progress - pbar.n)
 
         print(f"File '{file_name}' downloaded successfully to '{output_filepath}'")
 
@@ -200,3 +206,94 @@ def download_drive_file(auth_json_dict: dict, drive_file_url: str, output_filepa
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+def main():
+
+    try:
+        github_token_path = os.environ['GITHUB_TOKEN_PATH']
+    except ValueError as e:
+        msg = str(e)
+        sys.exit(msg + "\nSet GITHUB_TOKEN_PATH")
+    
+    github_token = None
+    try:
+        with open(github_token_path) as f:
+            github_token = f.read().strip()
+    except Exception as e:
+        sys.exit(str(e))
+    try:
+        assert github_token is not None
+    except AssertionError as e:
+        sys.exit(str(e))
+    
+    try:
+        gdrive_auth_path = os.environ['GDRIVE_AUTH_PATH']
+    except ValueError as e:
+        msg = str(e)
+        sys.exit(msg + '\nSet GDRIVE_AUTH_PATH.')
+
+    gdrive_auth = None
+    try:
+        with open(gdrive_auth_path) as f:
+            gdrive_auth = json.load(f)
+    except Exception as e:
+        sys.exit(str(e))
+    try:
+        assert isinstance(gdrive_auth, dict)
+    except AssertionError as e:
+        sys.exit(str(e))    
+    df = None
+    if github_token:
+        csv_bytes = fetch_private_github_file(github_token=github_token)
+        df = load_csv_from_bytes(csv_bytes)
+    else:
+        print("GitHub token not found.")
+
+    assert df is not None, sys.exit("Problem getting data from repo.")
+    
+    df = df[df['Materials'].str.contains('resin', na=False) & (df['Publish'] == True)].drop(columns=['Publish', 'Make time (3d printed)', 'Make time (handmade)', 'Make time (cast)', 'Image folder']).dropna()
+
+    os.makedirs('davedavepicks_stls', exist_ok=True)
+    try:
+        folder_id = os.environ['GDRIVE_FOLDER_ID']
+    except ValueError as e:
+        msg = str(e)
+        sys.exit(msg + '\nSet GDRIVE_FOLDER_ID.')
+        
+    ddp_stls = {}
+
+    ddp_stls = gdrive_stls(  #, ddp_items
+        auth_json_dict=gdrive_auth,
+        folder_id=folder_id,
+        recursive=True,
+        depth=3,
+    )
+
+    stldf = pd.DataFrame.from_dict(ddp_stls).drop(columns=['parents', 'mimeType'])
+    stldf['createdTime'] = pd.to_datetime(stldf['createdTime'], utc=True)
+    stldf['modifiedTime'] = pd.to_datetime(stldf['modifiedTime'], utc=True)
+    stldf['size'] = stldf['size'].astype('int32')
+    stldf.sort_values(by=['modifiedTime'], ascending=False, inplace=True)
+
+    # User decision making
+    os.system('rm -rf davedavepicks_stls')
+    for row in stldf.iterrows():
+        info = row[1]
+        # print(info)
+        print(f'Folder: {info["parent_folder"]}\n STL file: {info["name"]}\n Created: {info["createdTime"]}\n Modified: {info["modifiedTime"]}')  # noqa
+        keep = input('Do you want to opensource this STL? (y/n)')
+        if keep == 'y':
+            print(f'{info["name"]} will be opensourced.')
+            name = input(f'Enter new name? (currently {info["name"]}):')
+            if name == '':
+                name = info["name"]
+            elif '.stl' not in name:
+                name = name + '.stl'
+            folder = input(f'Enter new folder name? (currently {info["parent_folder"]}):')
+            if folder == '':
+                folder = info["folder"]
+            os.makedirs(f'davedavepicks_stls/{folder}', exist_ok=True)
+            download_drive_file(
+                auth_json_dict=gdrive_auth,
+                drive_file_url='https://drive.google.com/file/d/' + info["id"],
+                output_filepath=f"davedavepicks_stls/{folder}/{name}"
+            )
